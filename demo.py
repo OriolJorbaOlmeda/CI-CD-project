6b132bee020a7e349e5face9e80ecef50c40c7d7
Oriol <oriol.jorba.olmeda@gmail.com>
2021-12-12 15:20:58 +0100

    Se ha modificado demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+def sum(a, b):
+    for n in (a, b):
+        if not isinstance(n, int) and not isinstance(n, float):
+            raise TypeError
+    return a + b
